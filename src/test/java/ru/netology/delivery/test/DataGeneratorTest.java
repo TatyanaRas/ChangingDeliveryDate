666d8265e918bb0e03862356f006cadb89c5080b
Tatyana <tatyanalipch@yandex.ru>
2024-07-23 13:31:04 +0300

    build.gradle

--- src/test/java/ru/netology/delivery/test/DataGeneratorTest.java
+++ src/test/java/ru/netology/delivery/test/DataGeneratorTest.java
@@ -1,12 +1,10 @@
-package ru.netology.delivery.test;
+package ru.netology.delivery.data;
 
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.conditions.Text;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
-import ru.netology.delivery.data.DataGenerator;
 
 import java.time.Duration;
 
